3e2110a7f21816d05c74495deebef23708118b62
Tracy Lee <tracy@wingman.events>
2017-02-10 04:48:13 -0800

    setup more

--- src/app/banana/banana.component.ts
+++ src/app/banana/banana.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BananaDataService } from '../banana-data.service';
 
 @Component({
   selector: 'app-banana',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BananaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public bananaData: BananaDataService) { }
 
   ngOnInit() {
   }
---
+++ src/app/instructions-dialog/instructions-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instructions-dialog',
+  templateUrl: './instructions-dialog.component.html',
+  styleUrls: ['./instructions-dialog.component.css']
+})
+export class InstructionsDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
