15bcdeb41e46c3a17faea3f0185e12e0a6b1b0e8
Tracy Lee <tracy@wingman.events>
2017-02-10 04:35:36 -0800

    hopefully this is the last

--- src/app/home-data.service.ts
+++ src/app/home-data.service.ts
@@ -3,6 +3,43 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class HomeDataService {
 
+  puns = [{
+    type: "cat",
+    image: "cat-emoji.jpg",
+    altText: "cat emoji",
+    route: "meow",
+    chip1: "cat",
+    chip2: "meow",
+    chip3: "kitty"  
+  },
+  {
+    type: "banana",
+    image: "banana-emoji.jpg",
+    altText: "banana emoji",
+    route: "banana",
+    chip1: "banana",
+    chip2: "fruit",
+    chip3: "yellow"  
+  },
+  {
+    type: "cow",
+    image: "cow-emoji.jpg",
+    altText: "cow emoji",
+    route: "moo",
+    chip1: "cow",
+    chip2: "animal",
+    chip3: "moo"  
+  },
+  {
+    type: "duck",
+    image: "duck-emoji.jpg",
+    altText: "duck emoji",
+    chip1: "duck",
+    chip2: "quack",
+    chip3: "bird",
+    toolTip: "oops, this isn't done yet"  
+  }];
+
   constructor() { }
 
 }
